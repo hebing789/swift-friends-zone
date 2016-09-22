584a2bcbb130e014cf95ae4afb72d04781b758c6
1101918842@qq.com <1101918842@qq.com>
2016-09-22 20:43:53 +0800

    何兵项目初始化, 图标,启动图片,项目名称,横竖屏,打印log,类名前缀,设置,整体框架搭建

---
+++ weibo/weibo/Classes/View/Main/ViewControllers/HBTabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  HBTabBarController.swift
+//  weibo
+//
+//  Created by hebing on 16/9/22.
+//  Copyright © 2016年 hebing. All rights reserved.
+//
+
+import UIKit
+
+class HBTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor=UIColor.yellow
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+   
+}
